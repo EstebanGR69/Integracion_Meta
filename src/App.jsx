9aedf22da39fbff4d4f1d89c72e7abcab71890e1
EstebanGR69 <egalvan@dragontec.net>
2024-07-10 14:36:51 -0600

    xdxdxd

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,71 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
 
-  const initializeFacebookSDK = () => {
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '831868978888221',
-        xfbml      : true,
-        version    : 'v20.0'
-      });
-      FB.AppEvents.logPageView();
-      setIsSDKLoaded(true);
-      console.log("Facebook SDK initialized");
-    };
-
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-  };
-
-  useEffect(() => {
-    initializeFacebookSDK();
-  }, []);
-  if (window.FB) {
-    console.log("SDK already loaded",window.FB);
-    FB.getLoginStatus(function(response) {
-      console.log("Respuesta ",response);
-    });
-    return;
+  const facebookLoginHandler=(response)=>{
+    console.log("Respuesta ", response)
+    if(response.status=="connected"){
+        window.FB.api('/me?fields=id,name,email,picture',userData=>{})
+    }
   }
+
+  useEffect(()=>{
+    // Definir la inicialización de Facebook
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId: '831868978888221',
+          cookie: true,
+          xfbml: true,
+          version: 'v20.0'
+        });
+  
+        FB.AppEvents.logPageView();
+      };
+      // Cargar el SDK de Facebook
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+    },[])
+
+    var IniciarSesion=()=>{
+
+      /*Condicional para validar que existe el SDK de facebook*/
+        if(!window.FB){
+      console.log("NO SE CARGÓ EL SDK DE FACEBOOK")
+        }else{
+        console.log("SÍ SE CARGÓ EL SDK DE FACEBOOK")
+        console.log("SDK de facebook ",window.FB)
+        window.FB.login(facebookLoginHandler,{scope:'public_profile'})
+        /*Validar respuesta*/
+        window.FB.getLoginStatus(response=>{
+          /*En caso de conexión exitosa, se manejará la respuesta*/
+          if(response.status=="connected"){
+              window.FB.login(facebookLoginHandler)
+          }
+          /*En este caso hubo un problema con la app de meta*/
+          if(response.status=="unknown"){
+              window.FB.login(facebookLoginHandler)
+              console.log("Se cargó el SDK correctamente, pero hubo un error con la APP de meta")
+          }else {
+              console.log(response,"Usuario no autorizado")
+              window.FB.login(facebookLoginHandler)
+            }
+        })
+        }
+      }
   return (
     <div className="App">
-      <button onClick={initializeFacebookSDK}>
-          Cargar Facebook SDK
+      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center" onClick={IniciarSesion}>
+            Conectar con facebook
       </button>
-      {isSDKLoaded ? <p>Facebook SDK cargado</p> : <p>Cargando Facebook SDK...</p>}
+      
     </div>
   );
 }
